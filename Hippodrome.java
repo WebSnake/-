21b46f25cd5c69c173be765463b926e39bc7fd8e
Anton Salnikov <31500246+WebSnake@users.noreply.github.com>
2020-09-19 23:24:57 +0500

    Add files via upload

---
+++ Hippodrome.java
@@ -0,0 +1,60 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Hippodrome {
+    private List<Horse> horses = new ArrayList<>();
+    public static Hippodrome game;
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Horse> horses = new ArrayList<>();
+        horses.add(new Horse("Бублик", 3, 0));
+        horses.add(new Horse("Кралька", 3, 0));
+        horses.add(new Horse("Пончик", 3, 0));
+        game = new Hippodrome(horses);
+        game.run();
+        game.printWinner();
+    }
+
+    public Horse getWinner() {
+        Horse winner = horses.get(0);
+        for (Horse horse: horses) {
+            if (horse.getDistance() > winner.getDistance())
+                winner = horse;
+        }
+        return winner;
+    }
+    public void printWinner() {
+        System.out.println("Winner is " + getWinner().getName() + "!");
+    }
+
+    public void run() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+    }
+    public void move() {
+        for (Horse horse: horses) {
+            horse.move();
+        }
+    }
+    public void print() {
+        for (Horse horse: horses) {
+            horse.print();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+}
